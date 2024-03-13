857647355dab684544e551e9db5481a996a451a4
peter-tillmann88 <tillmann@my.yorku.ca>
2024-03-13 16:28:46 -0400

    deployed webstie

---
+++ src/Components/Homepage.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <h1>Welcome to My Personal Website</h1>
+    <p>Hello, I'm [Your Name] and I'm a [Your Profession].</p>
+    <p>This is where I'll share my work and thoughts with you.</p>
+  </div>
+);
+
+export default HomePage;
